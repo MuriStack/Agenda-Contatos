54a1827cedc98ec2f5e1984dffc59b9620b7bf18
Muri <murilo.belizario12@gmail.com>
2026-09-19 21:39:53 -0300

    Adicionando v1.1.1

--- src/br/edu/principal/Principal.java
+++ src/br/edu/principal/Principal.java
@@ -1,8 +1,12 @@
+package br.edu.principal;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
-	
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         List<String> nomes = new ArrayList<>();
         List<String> celulares = new ArrayList<>();
@@ -10,29 +14,37 @@ public class Principal {
 
         int opcao;
         boolean continuar = true;
-        
+
         Scanner sc = new Scanner(System.in);
-        
-        Uteis.mostraInicializacao(); 
+
+        Uteis.mostraInicializacao();
 
         while (continuar) {
-        	Uteis.mostraMenu();
+
+            Uteis.mostraMenu();
+
             opcao = Uteis.selecionaOpcao(sc);
 
-
-
             switch (opcao) {
-                case 1-> Agenda.adicionar(sc, nomes, celulares, emails);           	
-                case 2-> Agenda.listar(nomes, celulares, emails);
-                case 3-> Agenda.pesquisar(sc, nomes, celulares, emails);
-                case 4-> Agenda.atualizar(sc, nomes, celulares, emails);           	
-                case 5-> Agenda.excluir(sc, nomes, celulares, emails);          	
-                case 6-> Uteis.sair(continuar);
-                case 7-> Uteis.sobre();
+
+                case 1 -> Agenda.adicionar(sc, nomes, celulares, emails);
+
+                case 2 -> Agenda.listar(nomes, celulares, emails);
+
+                case 3 -> Agenda.pesquisar(sc, nomes, celulares, emails);
+
+                case 4 -> Agenda.atualizar(sc, nomes, celulares, emails);
+
+                case 5 -> Agenda.excluir(sc, nomes, celulares, emails);
+
+                case 6 -> continuar = Uteis.sair();
+
+                case 7 -> Uteis.sobre();
 
                 default -> System.out.println("Opção inválida!");
             }
         }
 
         sc.close();
-	}
+    }
+}
